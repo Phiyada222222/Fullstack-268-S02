33c943d904cdea58ee19d13109168193653496f7
Phiyada222222 <piyadachangnoi@gmail.com>
2026-01-15 16:03:52 +0700

    initail

---
+++ index.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 7000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
+
